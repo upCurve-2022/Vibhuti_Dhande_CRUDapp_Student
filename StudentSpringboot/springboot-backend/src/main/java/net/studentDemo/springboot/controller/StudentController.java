8fcd5ca70ef865a0401f3e07675645fcac84645c
Vibhuti Dhande <83495237+vibhutidhande@users.noreply.github.com>
2022-06-06 23:21:14 +0530

    Add files via upload

---
+++ StudentSpringboot/springboot-backend/src/main/java/net/studentDemo/springboot/controller/StudentController.java
@@ -0,0 +1,67 @@
+package net.studentDemo.springboot.controller;
+
+import net.studentDemo.springboot.exception.ResourceNotFoundException;
+import net.studentDemo.springboot.model.Student;
+import net.studentDemo.springboot.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @GetMapping
+    public List<Student> getAllStudents(){
+        return studentRepository.findAll();
+    }
+
+    // build create student REST API
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return studentRepository.save(student);
+    }
+
+    // build get student by id REST API
+    @GetMapping("{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable  long id){
+        Student student = studentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Student not exist with id:" + id));
+        return ResponseEntity.ok(student);
+    }
+
+    // build update student REST API
+    @PutMapping("{id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable long id, @RequestBody Student studentDetails) {
+        Student updateStudent = studentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Student not exist with id: " + id));
+
+        updateStudent.setFirstName(studentDetails.getFirstName());
+        updateStudent.setLastName(studentDetails.getLastName());
+        updateStudent.setEmailId(studentDetails.getEmailId());
+
+        studentRepository.save(updateStudent);
+
+        return ResponseEntity.ok(updateStudent);
+    }
+
+    // build delete student REST API
+    @DeleteMapping("{id}")
+    public ResponseEntity<HttpStatus> deleteStudent(@PathVariable long id){
+
+        Student student = studentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Student not exist with id: " + id));
+
+        studentRepository.delete(student);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+    }
+}
